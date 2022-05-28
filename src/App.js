4fdeadc15ca985b65f09b8313e5f5cc6f8f46559
Aleardo Kandiah <aleardotambi6@gmail.com>
2022-05-28 18:04:16 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ function App() {
   return (
     <div className="app-container">
       <h1>Poke evolution</h1>
+      <div className ='pokemon-container'>
+        <div className='all-container'>
+
+        </div>
+        <button className='load-more'>Load More</button>
+      </div>
     </div>
   );
 }
