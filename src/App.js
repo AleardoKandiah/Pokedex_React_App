826451ff2a20666397616fe379ca67e2330b94ab
Aleardo Kandiah <aleardotambi6@gmail.com>
2022-05-29 14:48:10 +0100

    third commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import { useState } from 'react';
 
 function App() {
+  
   const [allPokemons, setAllPokemons] = useState([])
+  const [LoadMore, setLoadMore] = useState('https://pokeapi.co/api/v2/pokemon?limit=20')
+  
+  const getAllPokemons = async () => {
+    const res = await fetch(LoadMore)
+  }
   return (
     <div className="app-container">
       <h1>Poke evolution</h1>
